94bfd9c75400cf75f72aed770e2ff5b6df4d82a7
Jan Zamostny <jan.zamostny04@gmail.com>
2025-03-24 10:19:35 +0100

    apps

--- APPS/apps-mcxn-workspace.mcu/test/source/main-test.cpp
+++ APPS/apps-mcxn-workspace.mcu/test/source/main-test.cpp
@@ -49,70 +49,78 @@ void _mcu_initialization()
 //! Global data
 
 //! LEDs on MCXN-KIT - instances of class DigitalOut
-DigitalOut g_led_P3_16( P3_16 );
-DigitalOut g_led_P3_17( P3_17 );
+//DigitalOut g_led_P3_16( P3_16 );
+//DigitalOut g_led_P3_17( P3_17 );
+//
+////! Button on MCXN-KIT - instance of class DigitalIn
+//DigitalIn g_but_P3_18( P3_18 );
+//DigitalIn g_but_P3_19( P3_19 );
+//DigitalIn g_but_P3_20( P3_20 );
+//DigitalIn g_but_P3_21( P3_21 );
 
-//! Button on MCXN-KIT - instance of class DigitalIn
-DigitalIn g_but_P3_18( P3_18 );
-DigitalIn g_but_P3_19( P3_19 );
-DigitalIn g_but_P3_20( P3_20 );
-DigitalIn g_but_P3_21( P3_21 );
+#define T 50
 
-//! Callback function for LED control
-void control_from_ticker()
+class LED
 {
-	static int l_ticks = 0;
+public:
+    DigitalOut m_led;
+    uint32_t m_T0;        // cas T0
 
-	int32_t l_periode = 500;					// T = 0.5 second
+  LED( uint32_t t_led_pin ) : m_led( t_led_pin )
+    {
+	  m_T0 = 0;
+    }
 
-	if ( g_but_P3_18.read() == 0 )				// check button
-	{
-		l_periode /= 10;						// increase speed of blinking
-	}
+    void nastav_jas_proc( uint8_t t_jas_proc )
+    {
+        m_T0 = T * ((double)t_jas_proc/100);
+    }
+};
 
-	if ( l_ticks < l_periode / 2 )				// time to switch on or off?
-	{
-		g_led_P3_16.write( 0 );					// LED Off
-	}
-	else
-	{
-		g_led_P3_16.write( 1 );					// LED On
-	}
+class BTN
+{
+public:
+	DigitalIn m_btn;
+	bool clicked;
 
-	l_ticks++;									// milliseconds counter
-	if ( l_ticks >= l_periode )					// end of time period?
+	BTN(uint32_t t_btn_pin) : m_led(t_btn_pin)
 	{
-		l_ticks = 0;							// start time period again
+		clicked = false;
 	}
+};
+
+LED g_red_led[] = { P3_16, P3_17 };
+
+void pwm_control()
+{
+  static int tick = 0;
+
+  if(tick < g_red_led[0].m_t0) {
+	  	 g_red_led[0].m_led.write(1);
+  } else {
+	  	 g_red_led[0].m_led.write(0);
+  }
+
+  if(tick < g_red_led[1].m_t0) {
+	  	 g_red_led[1].m_led.write(1);
+  } else {
+	  	 g_red_led[1].m_led.write(0);
+  }
+
+  tick++;
+
+  if(tick >= T) {
+	  tick = 0;
+  }
 }
-
-void test_leds();
-void test_lcd();
-void test_i2c();
 
 int main()
 {
-    PRINTF( "Testing program started!\r\n" );
 
-    // uncomment selected test
-    //	test_leds();
-    //	test_lcd();
-    //	test_i2c();
+  Ticker pwm1;
+  pwm1.attach( pwm_control );
+    g_red_led[ 0 ].nastav_jas_proc( 5 );
+    g_red_led[ 1 ].nastav_jas_proc( 50 );
 
-    // ******************************************************************
-
-    // default test for 2 LEDs and 4 buttons
-    while ( 1 )
-    {
-        g_led_P3_16.write( !g_led_P3_16.read() );	// invert current state
-        g_led_P3_17.write( !g_led_P3_16.read() );	// blink asynchronously
-
-        delay_ms( 250 );                      		// delay
-
-        while ( g_but_P3_18.read() == 0 ||
-        		g_but_P3_19.read() == 0 ||
-				g_but_P3_20.read() == 0 ||
-				g_but_P3_21.read() == 0 ); 			// stop blinking
-    }
+    while ( 1 ) __WFI();
 }
-
