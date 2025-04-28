ee64d5d911a9a165efb0ce9641599e2b3ae2ac51
Jan Zamostny <jan.zamostny04@gmail.com>
2025-04-28 10:14:33 +0200

    updated version of cuda4

---
+++ APPS/apps-cuda-demo/cuda4_unm/cuda_img.h
@@ -0,0 +1,56 @@
+// ***********************************************************************
+//
+// Demo program for education in subject
+// Computer Architectures and Parallel Systems.
+// Petr Olivka, dep. of Computer Science, FEI, VSB-TU Ostrava, 2020/11
+// email:petr.olivka@vsb.cz
+//
+// Example of CUDA Technology Usage.
+//
+// Image interface for CUDA
+//
+// ***********************************************************************
+
+#pragma once
+
+#include <opencv2/core/mat.hpp>
+
+// Structure definition for exchanging data between Host and Device
+struct CudaImg
+{
+  uint3 m_size;             // size of picture
+  union {
+      void   *m_p_void;     // data of picture
+      uchar1 *m_p_uchar1;   // data of picture
+      uchar3 *m_p_uchar3;   // data of picture
+      uchar4 *m_p_uchar4;   // data of picture
+  };
+
+  CudaImg(int height, int width, void *data_ptr) : m_p_void(data_ptr) {
+    this->m_size.y = height;
+    this->m_size.x = width;
+  }
+
+  __device__ void at3(int y, int x, uchar3 value) {
+    if(y >=0 && y < this->m_size.y && x >= 0 && x < this->m_size.x) {
+      this->m_p_uchar3[y*this->m_size.x+x] = value;
+    } else {
+      printf("Error: Index (%d, %d) is out of bounds.\n", y, x);
+    }
+  }
+  __device__ void at4(int y, int x, uchar4 value) {
+    if(y >=0 && y < this->m_size.y && x >= 0 && x < this->m_size.x) {
+      this->m_p_uchar4[y*this->m_size.x+x] = value;
+    } else {
+      printf("Error: Index (%d, %d) is out of bounds.\n", y, x);
+    }
+  }
+  __device__ void at1(int y, int x, uchar1 value) {
+    if(y >=0 && y < this->m_size.y && x >= 0 && x < this->m_size.x) {
+      this->m_p_uchar1[y*this->m_size.x+x] = value;
+    } else {
+      printf("Error: Index (%d, %d) is out of bounds.\n", y, x);
+    }
+  }
+};
+
---
+++ APPS/apps-cuda-demo/cuda4_unm/main4_unm.cpp
@@ -0,0 +1,76 @@
+// ***********************************************************************
+//
+// Demo program for education in subject
+// Computer Architectures and Parallel Systems.
+// Petr Olivka, dep. of Computer Science, FEI, VSB-TU Ostrava, 2020/11
+// email:petr.olivka@vsb.cz
+//
+// Example of CUDA Technology Usage with unified memory.
+//
+// Image transformation from RGB to BW schema. 
+// Image manipulation is performed by OpenCV library. 
+//
+// ***********************************************************************
+
+#include <stdio.h>
+#include <cuda_device_runtime_api.h>
+#include <cuda_runtime.h>
+#include <opencv2/opencv.hpp>
+
+#include "uni_mem_allocator.h"
+#include "cuda_img.h"
+
+namespace cv {
+}
+
+// Function prototype from .cu file
+void cu_run_grayscale( CudaImg t_bgr_cuda_img, CudaImg t_bw_cuda_img );
+void cu_insert_image( CudaImg &t_cuda_big_img, CudaImg &t_cuda_small_img, int2 t_pos );
+
+
+int main( int t_numarg, char **t_arg )
+{
+    // Uniform Memory allocator for Mat
+    UniformAllocator allocator;
+    cv::Mat::setDefaultAllocator( &allocator );
+
+    if ( t_numarg < 3 )
+    {
+        printf( "Enter 2 picture filenames!\n" );
+        return 1;
+    }
+
+    // Load image
+    cv::Mat large_img = cv::imread( t_arg[ 1 ], cv::IMREAD_COLOR ); // CV_LOAD_IMAGE_COLOR );
+    
+    if ( !large_img.data )
+    {
+        printf( "Unable to read file '%s'\n", t_arg[ 1 ] );
+        return 1;
+    }
+
+    cv::Mat small_img = cv::imread( t_arg[ 2 ], cv::IMREAD_COLOR );
+
+    if ( !small_img.data )
+    {
+        printf( "Unable to read file '%s'\n", t_arg[ 1 ] );
+        return 1;
+    }
+
+    // data for CUDA
+    CudaImg l_bgr_cuda_img(large_img.size().height, large_img.size().width, ( uchar3 * ) large_img.data), l_bw_cuda_img(small_img.size().height, small_img.size().width, ( uchar3 * ) small_img.data);
+    // l_bgr_cuda_img.m_size.x = l_bw_cuda_img.m_size.x = l_bgr_cv_img.size().width;
+    // l_bgr_cuda_img.m_size.y = l_bw_cuda_img.m_size.y = l_bgr_cv_img.size().height;
+    // l_bgr_cuda_img.m_p_uchar3 = ( uchar3 * ) l_bgr_cv_img.data;
+    // l_bw_cuda_img.m_p_uchar1 = ( uchar1 * ) l_bw_cv_img.data;
+
+    // Function calling from .cu file
+    // cu_run_grayscale( l_bgr_cuda_img, l_bw_cuda_img );
+    cu_insert_image( l_bgr_cuda_img, l_bw_cuda_img, {100, 100});
+
+    // Show the Color and BW image
+    // cv::imshow( "Color", l_bw_cv_img );
+    cv::imshow( "Color", large_img );
+    cv::waitKey( 0 );
+}
+
