b961b3850e5f05d0acff5c2abd8ce39de2acadaa
JaZafier00716 <jan.zamostny04@gmailcom>
2026-03-23 16:37:07 +0100

    cpp

---
+++ druhak/CPP/cv6/main2.cpp
@@ -0,0 +1,25 @@
+#include <print>
+
+class {
+
+  char* data = nullptr;
+  size_t size = 0;
+
+  union {
+    size_t reserved;
+    char buffer[16];
+  } data;
+
+  string() {
+    str = data.buffer;
+  }
+
+  char* get () {
+    return str;
+  }
+};
+
+
+int main () {
+  std::println("{}", sizeof(std::string));
+}
